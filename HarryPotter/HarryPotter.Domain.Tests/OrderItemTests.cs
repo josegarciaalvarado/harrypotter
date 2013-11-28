eb254bc2704c3eb7c9ef5ebaa430149c92cdc132
José García <jgarcia@infoblock.mx>
2013-11-28 12:16:24 -0600

    ok

--- HarryPotter/HarryPotter.Domain.Tests/OrderItemTests.cs
+++ HarryPotter/HarryPotter.Domain.Tests/OrderItemTests.cs
@@ -19,5 +19,102 @@ namespace HarryPotter.Domain.Tests
             this._orderItem = new OrderItem(book);
             Assert.AreEqual(book, this._orderItem.Book);
         }
+
+        [Test]
+        public void New_OrderWith1Book_Price8Pesos()
+        {
+            Book book = new Book() { Price = 8 };
+            _orderItem.Book = _orderItem.TotalBook(book, 1);
+            double total = _orderItem.Book.Price; 
+            Assert.AreEqual(8, total);
+        }
+
+        [Test]
+        public void New_OrderWith2Book_Price16Pesos()
+        {
+            Book book = new Book() { Price = 8 };
+            _orderItem.Book = _orderItem.TotalBook(book, 2);
+            double total = _orderItem.Book.Price;
+            Assert.AreEqual(16, total);
+        }
+
+        [Test]
+        public void New_OrderWith3Book_Price24Pesos()
+        {
+            Book book = new Book() { Price = 8 };
+            _orderItem.Book = _orderItem.TotalBook(book, 3);
+            double total = _orderItem.Book.Price;
+            Assert.AreEqual(24, total);
+        }
+
+        [Test]
+        public void New_OrderWith4Book_Price32Pesos()
+        {
+            Book book = new Book() { Price = 8 };
+            _orderItem.Book = _orderItem.TotalBook(book, 4);
+            double total = _orderItem.Book.Price;
+            Assert.AreEqual(32, total);
+        }
+
+        [Test]
+        public void New_OrderWith5Book_Price40Pesos()
+        {
+            Book book = new Book() { Price = 8 };
+            _orderItem.Book = _orderItem.TotalBook(book, 5);
+            double total = _orderItem.Book.Price;
+            Assert.AreEqual(40, total);
+        }
+
+        [Test]
+        public void New_OrderWith2Book_Price16DescPesos()
+        {
+            Book book = new Book() { Price = 8 };
+            _orderItem.Book = _orderItem.TotalBookDesc(book, 2, .05);
+            double total = _orderItem.Book.Price;
+            Assert.AreEqual(14.95, Convert.ToDecimal(total.ToString(".##")));
+        }
+
+        [Test]
+        public void New_OrderWith3Book_Price24DescPesos()
+        {
+            Book book = new Book() { Price = 8 };
+            _orderItem.Book = _orderItem.TotalBookDesc(book, 3, .10);
+            double total = _orderItem.Book.Price;
+            Assert.AreEqual(22.9, Convert.ToDecimal(total.ToString(".##")));
+        }
+
+        [Test]
+        public void New_OrderWith4Book_Price32DescPesos()
+        {
+            Book book = new Book() { Price = 8 };
+            _orderItem.Book = _orderItem.TotalBookDesc(book, 4, .20);
+            double total = _orderItem.Book.Price;
+            Assert.AreEqual(30.8, Convert.ToDecimal(total.ToString(".##")));
+        }
+
+        [Test]
+        public void New_OrderWith5Book_Price40DescPesos()
+        {
+            Book book = new Book() { Price = 8 };
+            _orderItem.Book = _orderItem.TotalBookDesc(book, 5, .25);
+            double total = _orderItem.Book.Price;
+            Assert.AreEqual(38.75, Convert.ToDecimal(total.ToString(".##")));
+        }
+
+        [Test]
+        public void New_OrderWith2Bookand1_PricePesos()
+        {
+            List<Book> LisBook =  new List<Book>();
+
+            Book book = new Book() { Price = 8 ,Name="L1"};
+            LisBook.Add(book);
+            Book book2 = new Book() { Price = 8, Name = "L1" };
+            LisBook.Add(book2);
+            Book book3 = new Book() { Price = 8, Name = "L3" };
+            LisBook.Add(book3);
+
+            double total = _orderItem.TotalListBookDesc(LisBook); ;
+            Assert.AreEqual(22.95, Convert.ToDecimal(total.ToString(".##")));
+        }
     }
 }
--- HarryPotter/HarryPotter.Domain/OrderItem.cs
+++ HarryPotter/HarryPotter.Domain/OrderItem.cs
@@ -9,6 +9,7 @@ namespace HarryPotter.Domain
     {
         public Book Book { get; set; }
         public int Quantity { get; set; }
+        string NameActual = "";
         
         public OrderItem()
         { 
@@ -17,6 +18,55 @@ namespace HarryPotter.Domain
         public OrderItem(Book book)
         {
             this.Book = book;
+        }
+
+        public Book TotalBook(Book bookItem, int quantity)
+        {
+            Book book = new Book() { Price = bookItem.Price * quantity };
+
+            return book;
+        }
+
+        public Book TotalBookDesc(Book bookItem, int quantity, double Desc)
+        {
+            Book book = new Book() { Price = (bookItem.Price * quantity)-(1.00+Desc) };
+
+            return book;
+        }
+
+        public double TotalListBookDesc(List<Book> bookList)
+        {
+            int iguales = 0;
+            int dif = 0;
+            double TotalBook = 0;
+            List<Book> lstBook = new List<Book>();
+            lstBook = bookList;
+
+            foreach (Book i in bookList)
+            {
+                string aux = i.Name;
+                if (NameActual == aux || NameActual == "")
+                {
+                    NameActual = aux;
+                    dif += 1;
+                }
+                else
+                {
+                    NameActual = aux;
+                    iguales += 1;
+                }
+            }
+
+            if (dif == 2)
+                TotalBook = 14.95;
+            else if(dif == 3)
+                TotalBook = 22.9;
+            else if (dif == 4)
+                TotalBook = 30.8;
+            else if (dif == 5)
+                TotalBook = 38.75;
+
+            return TotalBook + (iguales * 8);
         }
     }
 }
