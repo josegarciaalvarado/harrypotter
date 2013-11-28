9c533ca09fd0101d3cb2e570a30d0b08aef8aba1
José García <jgarcia@infoblock.mx>
2013-11-28 09:23:04 -0600

    Nuevos

---
+++ HarryPotter/HarryPotter.Data.Tests/DataContextTestsBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ImproveIT.Data;
+using NDbUnit.Core;
+using NUnit.Framework;
+using System.Configuration;
+
+namespace HarryPotter.Data.Tests
+{
+    public class DataContextTestsBase
+    {
+        internal IDataContext _dataContext;
+        internal INDbUnitTest _ndbUnitTest;
+
+        [SetUp]
+        public void Setup()
+        {
+            string cnn = ConfigurationManager.ConnectionStrings["storedb_development"].ConnectionString;
+            this._ndbUnitTest = new NDbUnit.Core.SqlClient.SqlDbUnitTest(cnn);
+            this._ndbUnitTest.ReadXmlSchema("StoreSchema.xsd");
+            this._ndbUnitTest.PerformDbOperation(NDbUnit.Core.DbOperationFlag.DeleteAll);
+
+            NHibernate.ISession session =
+                HarryPotter.Data.DataContextBuilder.BuildSession();
+            this._dataContext = new ImproveIT.Data.Hibernate.HibernateDataContext(session);
+        }
+    }
+}
